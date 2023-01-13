2f7197cdcde8d802ed73c01c26fae5715a73d559
Gustone <giannelli.richard@gmail.com>
2023-01-13 16:52:46 +0100

    SpringInizialSetUp

---
+++ src/main/java/com/project/projectUtilities/ProjectUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectUtilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/projectUtilities/ProjectUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectUtilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
